a8291750d22a1b7139a5ae9fe35d12e070eedd38
Samuel Estevez <julioestevez@aclanyc.com>
2019-03-28 22:25:11 +0000

    mine

--- js/game.js
+++ js/game.js
@@ -26,8 +26,8 @@ var game = {
             message: "You found a skeleton",
             choices: [
                 {
-                    text: "",
-                    nextLevel: "",
+                    text: "Fight",
+                    nextLevel: "deaded",
                 },
                 {
                     text: "Leave",
@@ -35,20 +35,107 @@ var game = {
                 }
             ]
         },
-
-        field: {
-            message: "You made wooden tools",
+        
+        deaded: {
+            message: "You died",
             choices: [
                 {
-                    text: "Mine",
-                    nextLevel: "",
-                },
-                {
-                    text: "",
-                    nextLevel: ""
+                    text: "Respawn",
+                    nextLevel: "start"
                 }
             ]
         },
 
+        field: {
+            message: "You got wood and made stone tools",
+            choices: [
+                {
+                    text: "Mine",
+                    nextLevel: "cave2",
+                }
+            ]
+        },
+        
+        cave2: {
+            message: "You found iron",
+            choices: [
+                {
+                    text: "Keep mining",
+                    nextLevel: "diamonds1",
+                },
+                {
+                    text: "Upgrade your pickaxe",
+                    nextLevel: "pickaxe"
+                }
+            ]
+        },
+        
+        diamonds1: {
+            message: "You found diamonds",
+            choices: [
+                {
+                    text: "Mine them",
+                    nextLevel: "f",
+                },
+                {
+                    text: "Come back later",
+                    nextLevel: "cave3"
+                }
+            ]
+        },
+        
+        f: {
+            message: "You tried mining but your pickaxe wasn't good enough. You lost the diamonds",
+            choices: [
+                {
+                    text: "Restart",
+                    nextLevel: "start",
+                }
+            ]
+        },
+        
+        pickaxe: {
+            message: "You made an iron pickaxe",
+            choices: [
+                {
+                    text: "Keep mining",
+                    nextLevel: "diamonds2",
+                }
+            ]
+        },
+        
+        diamonds2: {
+            message: "You found diamonds",
+            choices: [
+                {
+                    text: "Mine them",
+                    nextLevel: "diamonds",
+                },
+                {
+                    text: "Come back later",
+                    nextLevel: "cave3"
+                }
+            ]
+        },
+        
+        diamonds: {
+            message: "You got diamonds",
+            choices: [
+                {
+                    text: "Start over",
+                    nextLevel: "start",
+                }
+            ]
+        },
+        
+        cave3: {
+            message: "You got lost in the cave and fell in lava",
+            choices: [
+                {
+                    text: "Respawn",
+                    nextLevel: "start",
+                }
+            ]
+        },
     }
 };
